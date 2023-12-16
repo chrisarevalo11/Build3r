41d71393b78c1b9eb16ee3e0df26edd1f76d8ba1
Christian Arevalo <tiancris200@gmail.com>
2023-12-16 13:31:41 -0500

    :technologist: Implemented reusable ui components

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,17 @@
+import { DetailedHTMLProps } from "react";
+import { cn } from "@/lib/utils";
+
+type TButton = DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {};
+
+export function Button(props: TButton): JSX.Element {
+  const { children, className, ...rest } = props;
+
+  return (
+    <button className={cn("btn btn-primary", className)} {...rest}>
+      {children}
+    </button>
+  );
+}
---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,15 @@
+import { DetailedHTMLProps } from "react";
+import { cn } from "@/lib/utils";
+
+type TInput = DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {};
+
+export function Input(props: TInput): JSX.Element {
+  const { className, ...rest } = props;
+
+  return (
+    <input className={cn("input input-bordered w-full", className)} {...rest} />
+  );
+}
