39b3a5d9c01113dce17a29b57fd176a250e7d506
Christian Arevalo <tiancris200@gmail.com>
2023-12-17 23:54:01 -0500

    :technologist: Added container component

---
+++ src/components/ui/Container.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import { DetailedHTMLProps } from "react";
+
+type TContainer = DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+> & {};
+
+export default function Container(props: TContainer): JSX.Element {
+  const { className, children, ...rest } = props;
+
+  return (
+    <div
+      className={cn("w-full p-2 max-w-[1200px] mx-auto", className)}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
