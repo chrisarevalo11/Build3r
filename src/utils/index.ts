de5caf6f105639ffdf967dc6cb2803e714e9ed73
Christian Arevalo <tiancris200@gmail.com>
2023-12-20 18:23:08 -0500

    :technologist: Added dev utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,32 @@
 import { twMerge } from "tailwind-merge";
 import { clsx, ClassValue } from "clsx";
+import { BigNumber, ethers } from "ethers";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function toDecimal(amount: number): BigNumber {
+  return ethers.utils.parseEther(amount.toString());
+}
+
+export function formatAddress(address: string): string {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+}
+
+export function handleDate(date: number): string {
+  const dateObject: Date = new Date(date * 1000);
+
+  const year = dateObject.getFullYear();
+  const month = dateObject.getMonth() + 1;
+  const day = dateObject.getDate();
+
+  const dateFormatted = `${year}-${month}-${day}`;
+
+  return dateFormatted;
+}
+
+export function convertToUnixTime(dateStr: string): number {
+  const date = new Date(dateStr);
+  return Math.floor(date.getTime() / 1000);
 }
