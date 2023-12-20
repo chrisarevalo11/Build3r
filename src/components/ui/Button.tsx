fe517b7dedf2794399249fe4bd5099ca532657fe
Christian Arevalo <tiancris200@gmail.com>
2023-12-20 15:43:14 -0500

    :truck: Updated imports due to folder renaming

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,5 @@
 import { DetailedHTMLProps } from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils";
 
 type TButton = DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils";
 import { DetailedHTMLProps } from "react";
 
 type TContainer = DetailedHTMLProps<
