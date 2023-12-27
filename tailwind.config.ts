fcd2c8ad48e4b18fef94ce85030713eb4d076a97
Christian Arevalo <tiancris200@gmail.com>
2023-12-27 14:53:41 -0500

    :heavy_minus_sign: Removed daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,css,mdx}"],
-  theme: {
-    extend: {
-      colors: {
-        green400: "#1EB854",
-        green500: "#3f882b",
-        green600: "#2b5222",
-      },
-      animation: {
-        "spin-slow": "spin 20s linear infinite",
-      },
-    },
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["forest"],
-  },
-};
-export default config;
+	content: ['./src/**/*.{js,ts,jsx,tsx,css,mdx}'],
+	theme: {
+		extend: {
+			colors: {
+				green400: '#1EB854',
+				green500: '#3f882b',
+				green600: '#2b5222'
+			},
+			animation: {
+				'spin-slow': 'spin 20s linear infinite'
+			}
+		}
+	}
+}
+export default config
