2859e031962c9ff2fd147b6da5ab1953de21b6ea
Santiago Viana <salviega6@gmail.com>
2023-12-27 11:35:34 -0500

    :alembic: Disconect wallet retur landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 import Navbar from '@/components/Navbar/Navbar'
 
@@ -7,21 +7,19 @@ import Home from './pages/Home'
 
 function App(): JSX.Element {
 	return (
-		<>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/profile/:profileId' element={<></>} />
-					<Route path='/create' element={<Create />} />
-					<Route path='/explore' element={<></>}>
-						<Route path='projects/:projectId' element={<></>} />
-						<Route path='hypercerts/:hypercertId' element={<></>} />
-					</Route>
-					<Route path='*' element={<h1>404</h1>} />
-				</Routes>
-			</BrowserRouter>
-		</>
+		<HashRouter>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/profile/:profileId' element={<></>} />
+				<Route path='/create' element={<Create />} />
+				<Route path='/explore' element={<></>}>
+					<Route path='projects/:projectId' element={<></>} />
+					<Route path='hypercerts/:hypercertId' element={<></>} />
+				</Route>
+				<Route path='*' element={<h1>404</h1>} />
+			</Routes>
+		</HashRouter>
 	)
 }
 
