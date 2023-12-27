e39032c2c812280119f6a09ba8a1e94ad9398b72
Christian Arevalo <tiancris200@gmail.com>
2023-12-27 17:08:41 -0500

    :sparkles: Added shadcn components

---
+++ src/components/ui/container.tsx
@@ -0,0 +1,22 @@
+import { DetailedHTMLProps } from 'react'
+
+import { cn } from '@/utils'
+
+type TContainer = DetailedHTMLProps<
+	React.InputHTMLAttributes<HTMLDivElement>,
+	HTMLDivElement
+> &
+	object
+
+export function Container(props: TContainer): JSX.Element {
+	const { className, children, ...rest } = props
+
+	return (
+		<div
+			className={cn('w-full p-2 max-w-[1200px] mx-auto', className)}
+			{...rest}
+		>
+			{children}
+		</div>
+	)
+}
