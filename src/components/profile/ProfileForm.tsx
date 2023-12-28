7044503db8675266a440baa6577d3a11afd7f807
Christian Arevalo <tiancris200@gmail.com>
2023-12-28 14:20:38 -0500

    :iphone: Fixed responsive details

--- src/components/profile/ProfileForm.tsx
+++ src/components/profile/ProfileForm.tsx
@@ -86,7 +86,7 @@ export default function ProfileForm(): JSX.Element {
 	}
 	return (
 		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-2 mx-5'>
 				<FormField
 					control={form.control}
 					name='name'
