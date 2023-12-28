9acac0408312ad41d7831c233a6d0513dffbf17d
Christian Arevalo <tiancris200@gmail.com>
2023-12-28 11:42:46 -0500

    :sparkles: Implemented create profile form

--- src/components/profile/ProfileForm.tsx
+++ src/components/profile/ProfileForm.tsx
@@ -5,57 +5,198 @@ import { Button } from '@/components/ui/Button'
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
 	FormMessage
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { ETHEREUM_ADDRESSES_REGEX } from '@/constants/constans'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const formSchema = z.object({
-	username: z.string().min(2, {
-		message: 'Username must be at least 2 characters.'
+	name: z.string().min(2, {
+		message: 'Name must be at least 2 characters.'
+	}),
+	banner: z.string().min(1, {
+		message: 'Banner is required'
+	}),
+	logo: z.string().min(1, {
+		message: 'Logo is required'
+	}),
+	slogan: z.string().min(2, {
+		message: 'Slogan is required'
+	}),
+	website: z.string().url({
+		message: 'Invalid URL format for the website.'
+	}),
+	twitter: z.string().min(2, {
+		message: 'Twitter handle is required'
+	}),
+	description: z
+		.string()
+		.min(2, {
+			message: 'Description is required'
+		})
+		.max(200, {
+			message: 'Description must be less than 200 characters'
+		}),
+	members: z.string().refine(value => ETHEREUM_ADDRESSES_REGEX.test(value), {
+		message: 'Must be a valid Ethereum wallet or wallets separated by commas.'
 	})
 })
 
 export default function ProfileForm(): JSX.Element {
 	const form = useForm<z.infer<typeof formSchema>>({
 		defaultValues: {
-			username: ''
-		}
+			name: '',
+			banner: '',
+			logo: '',
+			slogan: '',
+			website: '',
+			twitter: '',
+			description: '',
+			members: ''
+		},
+		resolver: zodResolver(formSchema)
 	})
 
-	const onSubmit = async (data: z.infer<typeof formSchema>) => {
-		try {
-			// Perform any necessary actions with the form data
-			console.log('Form data:', data)
-		} catch (error) {
-			// Handle submission errors
-			console.error('Submission error:', error)
-		}
+	const onSubmit = (data: z.infer<typeof formSchema>) => {
+		console.log('Form data:', data)
 	}
 
 	return (
 		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
 				<FormField
 					control={form.control}
-					name='username'
+					name='name'
 					render={({ field }) => (
 						<FormItem>
-							<FormLabel>Username</FormLabel>
+							<FormLabel>Name</FormLabel>
 							<FormControl>
-								<Input placeholder='shadcn' {...field} />
+								<Input placeholder='My organization' {...field} />
 							</FormControl>
-							<FormDescription>
-								This is your public display name.
-							</FormDescription>
-							<FormMessage />
+							<FormMessage>{form.formState.errors.name?.message}</FormMessage>
 						</FormItem>
 					)}
 				/>
-				<Button type='submit'>Submit</Button>
+				<div className='flex flex-col md:flex-row md:gap-3'>
+					<FormField
+						control={form.control}
+						name='banner'
+						render={({ field }) => (
+							<FormItem className='grow'>
+								<FormLabel>Banner</FormLabel>
+								<FormControl>
+									<Input id='banner' type='file' {...field} />
+								</FormControl>
+								<FormMessage>
+									{form.formState.errors.banner?.message}
+								</FormMessage>
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='logo'
+						render={({ field }) => (
+							<FormItem className='grow'>
+								<FormLabel>Logo</FormLabel>
+								<FormControl>
+									<Input id='logo' type='file' {...field} />
+								</FormControl>
+								<FormMessage>{form.formState.errors.logo?.message}</FormMessage>
+							</FormItem>
+						)}
+					/>
+				</div>
+				<FormField
+					control={form.control}
+					name='slogan'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Slogan</FormLabel>
+							<FormControl>
+								<Input placeholder='Think Different' {...field} />
+							</FormControl>
+							<FormMessage>{form.formState.errors.slogan?.message}</FormMessage>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='website'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Website/linktree</FormLabel>
+							<FormControl>
+								<Input
+									type='url'
+									placeholder='https://www.mywebsite.org'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage>
+								{form.formState.errors.website?.message}
+							</FormMessage>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='twitter'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Twitter handle</FormLabel>
+							<FormControl>
+								<Input placeholder='johndoe123' {...field} />
+							</FormControl>
+							<FormMessage>
+								{form.formState.errors.twitter?.message}
+							</FormMessage>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='description'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Description</FormLabel>
+							<FormControl>
+								<Textarea
+									placeholder='My organization is focused on...'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage>
+								{form.formState.errors.description?.message}
+							</FormMessage>
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='members'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Members (wallets)</FormLabel>
+							<FormControl>
+								<Textarea placeholder='0x1234..., 0x5678...' {...field} />
+							</FormControl>
+							<FormMessage>
+								{form.formState.errors.members?.message}
+							</FormMessage>
+						</FormItem>
+					)}
+				/>
+				<div className='flex justify-center'>
+					<Button className='m-2' type='submit'>
+						Submit
+					</Button>
+				</div>
 			</form>
 		</Form>
 	)
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,10 @@
-import ProfileForm from '@/components/profile/ProfileForm'
+import CreateProfile from '@/components/profile/CreateProfile'
 import { Container } from '@/components/ui/container'
 
 export default function Profile(): JSX.Element {
 	return (
 		<Container className='flex flex-col gap-10 md:gap-4'>
-			<ProfileForm />
+			<CreateProfile />
 		</Container>
 	)
 }
