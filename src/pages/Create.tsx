5edb01d10a820ed75cf8f14c0f59d827dbcdeceb
Christian Arevalo <tiancris200@gmail.com>
2023-12-28 14:21:28 -0500

    :sparkles: Implemented create grant hero section

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import CreateHero from '@/components/create/CreateHero'
 import Hypercert from '@/components/create/Hypercert'
 import ProjectForm from '@/components/create/ProjectForm'
 import { Container } from '@/components/ui/container'
@@ -21,14 +22,14 @@ export default function Create(): JSX.Element {
 	const [formValues, setFormValues] = useState<FormValuesTypes>(initialValue)
 
 	return (
-		<Container className='flex flex-col justify-evenly items-center my-3 w-full gap-4 md:gap-8'>
-			<h1 className='text-green400 font-bold text-4xl md:text-5xl my-3 text-center'>
-				Create a project
-			</h1>
-			<div className='flex flex-col-reverse gap-5 lg:gap-0 w-full lg:flex-row justify-evenly items-center lg:items-start'>
-				<ProjectForm formValues={formValues} setFormValues={setFormValues} />
-				<Hypercert formValues={formValues} />
-			</div>
-		</Container>
+		<>
+			<CreateHero />
+			<Container className='flex flex-col justify-evenly items-center my-3 w-full gap-4 md:gap-8'>
+				<div className='flex flex-col-reverse gap-5 lg:gap-0 w-full lg:flex-row justify-evenly items-center lg:items-start'>
+					<ProjectForm formValues={formValues} setFormValues={setFormValues} />
+					<Hypercert formValues={formValues} />
+				</div>
+			</Container>
+		</>
 	)
 }
