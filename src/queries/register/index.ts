b62ac6eedfe6c9e1393aecbe78d97dd35600038d
Santiago Viana <salviega6@gmail.com>
2023-12-28 10:23:05 -0500

    :sparkles: New query

--- src/queries/register/index.ts
+++ src/queries/register/index.ts
@@ -5,3 +5,21 @@ export const queryProfilesWhereOwner = `
       }
     }
   `
+
+export const queryPaginatedProfiles = `
+    query Profiles($first: Int!, $skip: Int!) {
+      profiles(orderBy: createdAt, orderDirection: desc, first: $first, skip: $skip) {
+        id
+        metadata {
+         pointer
+          protocol
+        }
+        nonce
+        name
+        owner {
+          id
+        }
+        anchor
+      }
+    }
+  `
