071d52aaaa248d257eedb4857830cf501bf07cac
Christian Arevalo <tiancris200@gmail.com>
2023-12-29 23:08:51 -0500

    :speech_balloon: Added title and description to form

--- src/pages/CreateProfile.tsx
+++ src/pages/CreateProfile.tsx
@@ -46,7 +46,24 @@ export default function CreateProfile(): JSX.Element {
 				<Oval />
 			) : (
 				<>
-					<h1 className='text-3xl font-bold'>Create Profile</h1>
+					<div className='w-full md:w-1/2 mx-auto text-center space-y-3'>
+						<h1 className='text-5xl font-soria text-primary'>Create Profile</h1>
+						<p className=' md:text-lg'>
+							If you want to register your{' '}
+							<span className='text-primary font-soria font-extrabold tracking-wide text-lg'>
+								organization
+							</span>{' '}
+							in order to{' '}
+							<span className='text-primary font-soria font-extrabold tracking-wide text-lg'>
+								create grants
+							</span>{' '}
+							and get to know the people who will help you{' '}
+							<span className='text-primary font-soria font-extrabold tracking-wide text-lg'>
+								achieving your goals
+							</span>
+							, fill out this form...
+						</p>
+					</div>
 					<div className='grid grid-cols-1 md:grid-cols-2 items-center relative overflow-hidden'>
 						<img
 							className='absolute md:static z-[-1] -right-[40%] w-[150%] md:w-full my-auto'
