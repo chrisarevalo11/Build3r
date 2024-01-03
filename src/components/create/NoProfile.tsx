d06e195c9feac76712137c7e77fd6f6bf52b928b
Christian Arevalo <tiancris200@gmail.com>
2024-01-03 00:04:26 -0500

    :children_crossing: Added no profile page on /create

---
+++ src/components/create/NoProfile.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from 'react-router-dom'
+
+import { Button } from '@/components/ui/Button'
+import { Container } from '@/components/ui/container'
+
+export default function NoProfile(): JSX.Element {
+	const navigate = useNavigate()
+
+	return (
+		<Container className='grid md:grid-cols-2 justify-items-center items-center gap-10 md:px-4 my-10'>
+			<img
+				className='md:order-2 w-[60%] md:w-[90%] object-cover'
+				src={'/images/no-profile.webp'}
+				alt='create profile'
+			/>
+			<div className='space-y-4 flex flex-col items-center md:items-start'>
+				<div className='text-center md:text-left'>
+					<h1 className='font-soria text-primary text-5xl font-bold'>Ooops!</h1>
+					<p className='text-lg text-pretty'>
+						Seems that you don&apos;t have a profile. In order to create grants,
+						you need to{' '}
+						<span className='text-primary font-bold font-soria text-xl tracking-wide'>
+							create a profile
+						</span>
+					</p>
+				</div>
+				<Button onClick={() => navigate('/profile/create')}>
+					Create Profile
+				</Button>
+			</div>
+		</Container>
+	)
+}
--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -6,8 +6,8 @@ import { useAccount } from 'wagmi'
 import CreateHero from '@/components/create/CreateHero'
 import GrantCard from '@/components/create/GrantCard'
 import GrantForm from '@/components/create/GrantForm'
+import NoProfile from '@/components/create/NoProfile'
 import { Container } from '@/components/ui/container'
-// import { FProfileDto } from '@/models/profile.model'
 import { AppDispatch, useAppSelector } from '@/store'
 import { getProfile } from '@/store/thunks/profile.thunk'
 import { grantFormValuesTypes } from '@/types'
@@ -18,7 +18,7 @@ export default function Create(): JSX.Element {
 
 	const dispatch = useDispatch<AppDispatch>()
 	const profileDto = useAppSelector(state => state.profileSlice.profileDto)
-	// const fetched = useAppSelector(state => state.profileSlice.profileFetched)
+	const fetched = useAppSelector(state => state.profileSlice.profileFetched)
 
 	const initialValue: grantFormValuesTypes = {
 		name: '',
@@ -40,6 +40,10 @@ export default function Create(): JSX.Element {
 
 		dispatch(getProfile(address as string))
 	}, [address, navigate, dispatch])
+
+	if (fetched && profileDto.id === '') {
+		return <NoProfile />
+	}
 
 	return (
 		<section className='flex flex-col gap-10 lg:gap-[2rem]'>
