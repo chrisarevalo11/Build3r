f1ff1f8981b051da6faaeaac425f7544157ebc8c
Christian Arevalo <tiancris200@gmail.com>
2024-01-04 10:20:46 -0500

    :construction: Stepper in progress

---
+++ src/components/grants/AddRecipientForm.tsx
@@ -0,0 +1,215 @@
+import { BytesLike, ethers } from 'ethers'
+import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux'
+import * as z from 'zod'
+
+import { Button } from '@/components/ui/Button'
+import { DialogFooter } from '@/components/ui/dialog'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { ARBITRUM_RECIPIENT_WALLET } from '@/constants/constans'
+import { fRecipientSubmitionDtoToFRecipientSubmition } from '@/functions/dtos/recipient.dtos'
+import { FPoolDto } from '@/models/pool.model'
+import { FProfileDto } from '@/models/profile.model'
+import { FRecipientSubmitionDto } from '@/models/recipient.model'
+import { AppDispatch } from '@/store'
+import { setLoading } from '@/store/slides/uiSlice'
+import { addRecipient } from '@/store/thunks/recipient.thunk'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+type Props = {
+	poolDto: FPoolDto
+	profileDto: FProfileDto
+	setStep: () => void
+}
+
+const formSchema = z.object({
+	fullName: z.string().min(1, {
+		message: 'Name is required'
+	}),
+	bio: z.string().min(1, {
+		message: 'Bio is required'
+	}),
+	organization: z.string().min(1, {
+		message: 'Organization is required'
+	}),
+	email: z.string().email({
+		message: 'Invalid email format'
+	}),
+	image: z.string().min(1, {
+		message: 'image is required'
+	})
+})
+
+export default function AddRecipientForm(props: Props): JSX.Element {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const { poolDto, profileDto, setStep } = props
+	const dispatch = useDispatch<AppDispatch>()
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			fullName: '',
+			bio: '',
+			organization: '',
+			email: '',
+			image: ''
+		}
+	})
+
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		setStep()
+		console.log(values)
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const onAddRecipient = async () => {
+		dispatch(setLoading(true))
+		const fullname: string = 'Santiago Viana'
+		const bio: string = 'I am a software developer'
+		const organization = 'Wagmi'
+		const email: string = 'salviega6@gmail.com'
+		const wallet: string = ARBITRUM_RECIPIENT_WALLET
+		const grantAmount: number = 20
+		const imageFile: string =
+			'https://avatars.githubusercontent.com/u/24712956?v=4'
+
+		if (!imageFile) {
+			alert('Error: image isimageFile required')
+			dispatch(setLoading(false))
+			return
+		}
+
+		const frecipientSubmisionDto: FRecipientSubmitionDto = {
+			anchor: profileDto.anchor,
+			bio,
+			email,
+			fullname,
+			grantAmount,
+			image: imageFile,
+			organization,
+			wallet
+		}
+
+		const frecipientSubmission: BytesLike =
+			await fRecipientSubmitionDtoToFRecipientSubmition(frecipientSubmisionDto)
+
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const ethereum = (window as any).ethereum
+
+		const web3Provider: ethers.BrowserProvider = new ethers.BrowserProvider(
+			ethereum
+		)
+		await web3Provider.send('eth_requestAccounts', [])
+		const web3Signer: ethers.JsonRpcSigner = await web3Provider.getSigner()
+
+		dispatch(
+			addRecipient({
+				frecipientSubmition: frecipientSubmission,
+				frecipientDtoWallet: frecipientSubmisionDto.wallet,
+				poolId: poolDto.id,
+				providerOrSigner: web3Signer
+			})
+		)
+	}
+
+	return (
+		<div>
+			<h1 className='text-xl font-bold text-primary mb-2'>Set recipient</h1>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-1'>
+					<FormField
+						control={form.control}
+						name='fullName'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Full Name</FormLabel>
+								<FormControl>
+									<Input placeholder='John Doe' {...field} />
+								</FormControl>
+								<FormMessage>
+									{form.formState.errors.fullName?.message}
+								</FormMessage>
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='bio'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Bio</FormLabel>
+								<FormControl>
+									<Input placeholder='John Doe' {...field} />
+								</FormControl>
+								<FormMessage>{form.formState.errors.bio?.message}</FormMessage>
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='organization'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Organization</FormLabel>
+								<FormControl>
+									<Input placeholder='John Doe' {...field} />
+								</FormControl>
+								<FormMessage>
+									{form.formState.errors.organization?.message}
+								</FormMessage>
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email</FormLabel>
+								<FormControl>
+									<Input placeholder='John Doe' {...field} />
+								</FormControl>
+								<FormMessage>
+									{form.formState.errors.email?.message}
+								</FormMessage>
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='image'
+						render={({ field }) => (
+							<FormItem className='grow'>
+								<FormLabel>image</FormLabel>
+								<FormControl>
+									<Input
+										className='cursor-pointer'
+										type='file'
+										accept='image/*'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage>
+									{form.formState.errors.image?.message}
+								</FormMessage>
+							</FormItem>
+						)}
+					/>
+					<DialogFooter>
+						<Button className='mt-2' type='submit'>
+							Submit
+						</Button>
+					</DialogFooter>
+				</form>
+			</Form>
+		</div>
+	)
+}
---
+++ src/components/grants/RecipientsSteps.tsx
@@ -0,0 +1,132 @@
+import { useState } from 'react'
+
+import AddRecipientForm from '@/components/grants/AddRecipientForm'
+import { FPoolDto } from '@/models/pool.model'
+import { FProfileDto } from '@/models/profile.model'
+import { FRecipientSubmitionDto } from '@/models/recipient.model'
+
+import { Button } from '../ui/Button'
+import { DialogFooter } from '../ui/dialog'
+
+type Props = {
+	poolDto: FPoolDto
+	profileDto: FProfileDto
+}
+export default function RecipientSteps(props: Props) {
+	const { poolDto, profileDto } = props
+	const [steps, setStep] = useState({
+		stepsCount: [0, 1, 2],
+		currentStep: 0
+	})
+
+	const handleNextStep = () => {
+		setStep({
+			...steps,
+			currentStep: steps.currentStep + 1
+		})
+	}
+
+	const stepContent: JSX.Element[] = [
+		<AddRecipientForm
+			key={0}
+			poolDto={poolDto}
+			profileDto={profileDto}
+			setStep={handleNextStep}
+		/>,
+		// <RegisterRecipients key={1} recipient={poolDto.recipients[0]} />,
+		<h1 key={2}></h1>
+	]
+
+	return (
+		<>
+			<div className='w-[95%] mx-auto px-4 sm:px-0 mt-4'>
+				<ul aria-label='Steps' className='flex items-center'>
+					{steps.stepsCount.map((item, index) => (
+						<li
+							key={index}
+							aria-current={steps.currentStep === index ? 'step' : false}
+							className='flex-1 last:flex-none flex items-center'
+						>
+							<div className='flex flex-col items-center'>
+								<div
+									className={`size-6 rounded-full border-2 flex-none flex items-center justify-center ${
+										steps.currentStep > index
+											? 'bg-primary border-primary'
+											: '' || steps.currentStep === index
+												? 'border-primary'
+												: ''
+									}`}
+								>
+									<span
+										className={`size-2 rounded-full bg-primary ${
+											steps.currentStep !== index ? 'hidden' : ''
+										}`}
+									></span>
+									{steps.currentStep > index ? (
+										<svg
+											xmlns='http://www.w3.org/2000/svg'
+											fill='none'
+											viewBox='0 0 24 24'
+											strokeWidth={1.5}
+											stroke='currentColor'
+											className='size-4 text-white'
+										>
+											<path
+												strokeLinecap='round'
+												strokeLinejoin='round'
+												d='M4.5 12.75l6 6 9-13.5'
+											/>
+										</svg>
+									) : (
+										''
+									)}
+								</div>
+							</div>
+							<hr
+								className={`w-[90%] mx-auto border ${
+									index === steps.stepsCount.length - 1
+										? 'hidden'
+										: '' || steps.currentStep > index
+											? 'border-primary'
+											: ''
+								}`}
+							/>
+						</li>
+					))}
+				</ul>
+			</div>
+			<div className='mt-4'>{stepContent[steps.currentStep]}</div>
+		</>
+	)
+}
+
+type RegisterRecipientsProps = {
+	recipient: FRecipientSubmitionDto
+	// setStep: () => void
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function RegisterRecipients(props: RegisterRecipientsProps): JSX.Element {
+	const { recipient } = props
+	const { fullname, email, organization, bio } = recipient
+
+	return (
+		<div>
+			<h1 className='text-xl font-bold text-primary mb-2'>
+				Register Recipients
+			</h1>
+			<div className='w-full border-border border-2 rounded-xl p-1 flex flex-col'>
+				<h2>
+					{fullname} - {email}
+				</h2>
+				<p>{organization}</p>
+				<p>{bio}</p>
+			</div>
+			<DialogFooter>
+				<Button className='mt-2' onClick={() => {}}>
+					Register
+				</Button>
+			</DialogFooter>
+		</div>
+	)
+}
